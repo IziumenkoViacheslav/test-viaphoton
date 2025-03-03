8f330ca99530b4905a2757e07be5a3963002a5c1
IziumenkoViacheslav <2744097@gmail.com>
2025-03-03 16:42:11 +0100

    maxKgNutsCanTranspoted function added to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,40 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function maxKgNutsCanTranspoted(D: number, N: number, F: number, C: number): number {
+  console.log('maxKgNutsCanTranspoted started');
+
+  console.log({ D, N, F, C });
+  let nutsInKg = N;
+  const distanseInKm = D;
+  const fuelKgPerKm = F;
+  const cartCapacityInKg = C;
+  let result = 0;
+  const fuelForOneTripInKg = distanseInKm * fuelKgPerKm;
+  console.log({ fuelForOneTripInKg });
+
+  const fuelForOneRoundTripInKg = fuelForOneTripInKg * 2;
+  console.log({ fuelForOneRoundTripInKg });
+
+  const enouphFuelForOneTrip = nutsInKg >= fuelForOneTripInKg;
+  console.log({ enouphFuelForOneTrip });
+
+  while (nutsInKg > 0) {
+    nutsInKg -= fuelForOneRoundTripInKg;
+    console.log({ nutsInKg });
+
+    console.log({ cartCapacityInKg });
+
+    if (nutsInKg > cartCapacityInKg) {
+      result += cartCapacityInKg;
+      nutsInKg -= cartCapacityInKg;
+    } else { // last trip
+      result += nutsInKg;
+    }
+  }
+  console.log({ result });
+
+  return result;
+
+}
